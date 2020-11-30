af458fe7b2088ce814c7a7257c7e50b8fcc09968
RyuMurase <murase.ryu.86c@gmail.com>
2020-11-30 11:23:35 +0900

    introduced COL number

--- Imaging_search_event.cpp
+++ Imaging_search_event.cpp
@@ -2,6 +2,16 @@
 
 #include "./library.hpp"
 
+const int COL_FILE = 0;
+const int COL_FRAME = 1;
+const int COL_X = 2;
+const int COL_Y = 3;
+const int COL_MOLAXIS = 4;
+
+const int X_C = 270;
+const int Y_C = 1020;
+const int R = 10;
+
 int main()
 {
     cout << "************************************************" << endl;
@@ -9,14 +19,16 @@ int main()
 	cout << "**        ver.2020.09.25 written by R. Murase **" << endl;
 	cout << "************************************************" << endl << endl;
     
-    string inputDataPath = "E:\\Cluster_sputtering\\MT2020\\MT2020#4\\900keVC4_pos\\imaging\\7_Linear_molAxis\\20200919_q=1122.txt";
+    string inputDataPath = "E:\\Cluster_sputtering\\MT2020\\MT2020#4\\900keVC2_pos\\imaging\\6_molAxis\\20200916_q=12_r0=1.27.txt";
     auto inputListData = readListFile<double> (inputDataPath);
     for (int i = 0; i < inputListData.size(); i++)
     {
-        double t =  inputListData[i][4];
-        if(t < 10)
+        double x = inputListData[i][COL_X];
+        double y = inputListData[i][COL_Y];
+        double t = inputListData[i][COL_MOLAXIS];
+        if(t < 1 && (x-X_C)*(x-X_C) + (y-Y_C)*(y-Y_C) < R*R)
         {
-            cout << setprecision(10) << inputListData[i][0] << " " << inputListData[i][1] << " " << inputListData[i][4] << endl;
+            cout << setprecision(10) << inputListData[i][COL_FILE] << " " << inputListData[i][COL_FRAME] << " " << inputListData[i][COL_MOLAXIS] << endl;
         }
     }
     return 0;
