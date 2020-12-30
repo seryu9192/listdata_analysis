69e1535f92efbbe77ef9f30a5caab119fee7d53d
RyuMurase <murase.ryu.86c@gmail.com>
2020-12-30 15:57:50 +0900

    The first commit make to count event of list in folder

---
+++ event_counter.py
@@ -0,0 +1,39 @@
+# event_counter.py: そのフォルダにある
+
+import os
+
+
+def main():
+    print('**** event_counter.py  ****')
+    #working directory
+    working_directory = input('Input the working directory\n --> ')
+    outputpath = os.path.join(working_directory, 'event_counts.txt')
+    outputdata = ''
+    para_tot = 0
+    perp_tot = 0
+    else_tot = 0
+    for filename in os.listdir(working_directory):
+        #avoid to open directory or event.txt (which you are going to make) file
+        if '.txt' not in filename or 'event_counts' in filename:
+            continue
+        with open(os.path.join(working_directory, filename), 'r') as f:
+            inputdat = f.read()
+        
+        #get line number by counting the number of '\n'
+        event_counts = inputdat.count('\n')
+        #compose output data
+        outputdata += filename + '\t' + str(event_counts) + '\n'
+        if '-30.0' in filename:
+            para_tot += event_counts
+        elif '-90.0' in filename:
+            perp_tot += event_counts
+        else:
+            else_tot += event_counts
+    outputdata += 'para_tot\t' + str(para_tot) + '\n'
+    outputdata += 'perp_tot\t' + str(perp_tot) + '\n'
+    outputdata += 'else_tot\t' + str(else_tot) + '\n'
+    with open(outputpath, 'w') as f:
+        f.write(outputdata)
+
+if __name__ == '__main__':
+    main()
