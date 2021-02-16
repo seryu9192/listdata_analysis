c4323e630aca9440c6a0cba47ffa9453b11ef760
RyuMurase <murase.ryu.86c@gmail.com>
2021-02-16 14:43:54 +0900

    change: 'else' to 'mid'
    add data condition to the output filename

--- Imaging_#_event_counter.py
+++ Imaging_#_event_counter.py
@@ -5,13 +5,14 @@ import os
 
 def main():
     print('**** Both_#_event_counter.py  ****')
-    #working directory
+    #working directory(wd)
     working_directory = input('Input the working directory\n --> ')
-    outputpath = os.path.join(working_directory, 'event_counts.txt')
     outputdata = ''
     para_tot = 0
     perp_tot = 0
-    else_tot = 0
+    mid_tot = 0
+
+    #loop for filename in the wd
     for filename in os.listdir(working_directory):
         #avoid to open directory or event.txt (which you are going to make) file
         if '.txt' not in filename or 'event_counts' in filename:
@@ -23,15 +24,21 @@ def main():
         event_counts = inputdat.count('\n')
         #compose output data
         outputdata += filename + '\t' + str(event_counts) + '\n'
-        if '-30.0' in filename:
+        if '0.0-30.0' in filename:
             para_tot += event_counts
-        elif '-90.0' in filename:
+        elif '60.0-90.0' in filename:
             perp_tot += event_counts
-        else:
-            else_tot += event_counts
+        elif '30.0-60.0' in filename:
+            mid_tot += event_counts
+    #add to output text
     outputdata += 'para_tot\t' + str(para_tot) + '\n'
     outputdata += 'perp_tot\t' + str(perp_tot) + '\n'
-    outputdata += 'else_tot\t' + str(else_tot) + '\n'
+    outputdata += 'mid_tot\t' + str(mid_tot) + '\n'
+
+    #write to the outputfile
+    data_cond = working_directory.split('\\')[4]
+    print(data_cond)
+    outputpath = os.path.join(working_directory, data_cond + '_event_counts.txt')
     with open(outputpath, 'w') as f:
         f.write(outputdata)
     print(f'done --- outputfile: {outputpath}')
